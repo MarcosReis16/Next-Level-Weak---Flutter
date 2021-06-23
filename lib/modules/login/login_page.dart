0a3b6f4ad3b04df5a89e56817e3e11adec39d312
Marcos Reis <marcossummers@gmail.com>
2021-06-23 20:38:27 -0300

    Finalizando Aula 3

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -3,6 +3,7 @@ import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/themes/appcolors.dart';
 import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
+import 'login_controller.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -64,7 +67,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     child: SocialLoginButton(
                       onTap: () {
-                        print("clicou");
+                        controller.googleSignIn(context);
                       },
                     ),
                   ),
--- lib/shared/themes/appcolors.dart
+++ lib/shared/themes/appcolors.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static final primary = Color(0xFFFF941A);
-  static final gray = Color(0xFF585666);
-  static final delete = Color(0xFF585666);
-  static final heading = Color(0xFF585666);
-  static final body = Color(0xFF706E7A);
-  static final stroke = Color(0xFFE3E3E6);
-  static final shape = Color(0xFFFAFAFC);
-  static final background = Color(0xFFFFFFFF);
+  static final primary = const Color(0xFFFF941A);
+  static final grey = const Color(0xFF585666);
+  static final delete = const Color(0xFF585666);
+  static final heading = const Color(0xFF585666);
+  static final body = const Color(0xFF706E7A);
+  static final stroke = const Color(0xFFE3E3E6);
+  static final shape = const Color(0xFFFAFAFC);
+  static final background = const Color(0xFFFFFFFF);
 }
