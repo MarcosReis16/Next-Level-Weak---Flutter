984b2251c2e186940d311cf8eb1d555a4d5ded22
Marcos Reis <marcossummers@gmail.com>
2021-06-24 21:14:50 -0300

    Metade da aula 4

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/appcolors.dart';
@@ -13,12 +14,14 @@ class AppWidget extends StatelessWidget {
       title: 'Pay Flow',
       theme: ThemeData(
         primaryColor: AppColors.primary,
+        primarySwatch: Colors.orange,
       ),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
